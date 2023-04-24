650683742339fbdd063cafbbb28ee8b81363709d
Krivtsov Pavel <pav.krivtsov@gmail.com>
2023-04-24 17:37:28 +0300

    initial: add empty project

---
+++ MobileUpGallery/MobileUpGallery/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MobileUpGallery
+//
+//  Created by Pavlentiy on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
